c76ea1c5cd89ace3ddba0642a14792ee07aba485
akikan-alumi <y_r9fj22sdf@live.jp>
2015-04-30 00:08:15 +0900

    ボタンなどを作成した時はメソッドが生成されるが、通常はそれに関わる子メソッドを更に作成しておいたほうがいいかもしれない。

--- TRPGSupportTool/TRPGSupportTool/SubDisplay.Designer.cs
+++ TRPGSupportTool/TRPGSupportTool/SubDisplay.Designer.cs
@@ -28,14 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // SubDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(407, 262);
             this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "SubDisplay";
             this.Text = "操作画面";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -44,6 +57,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
 
     }
 }
--- TRPGSupportTool/TRPGSupportTool/SubDisplay.cs
+++ TRPGSupportTool/TRPGSupportTool/SubDisplay.cs
@@ -8,26 +8,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace TRPGSupportTool
-{
-    public partial class SubDisplay : Form
-    {
-        public SubDisplay()
-        {
+namespace TRPGSupportTool {
+    public partial class SubDisplay : Form {
+        public SubDisplay() {
             InitializeComponent();
             int displayHeight = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
             int displayWidth = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
             this.StartPosition = FormStartPosition.Manual;
             this.DesktopLocation = new Point(displayHeight / 2, displayWidth / 6);
-            this.TopMost = true;
-
+            this.TopMost = false;//
+            this.ShowInTaskbar = false;// このフォームをタスクバーに表示しない
         }
 
-        private void Form2_Load(object sender, EventArgs e)
-        {
+        private void Form2_Load(object sender, EventArgs e) {
+            
+        }
 
-            // このフォームをタスクバーに表示しない
-            this.ShowInTaskbar = false;
+        private void button1_Click(object sender, EventArgs e) {
+
         }
     }
 }
