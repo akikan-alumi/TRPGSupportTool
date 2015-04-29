cc1bd03f27249363a84dc5bfa71b24ccba8bae1e
akikan-alumi <y_r9fj22sdf@live.jp>
2015-04-29 20:56:50 +0900

    first commit

---
+++ TRPGSupportTool/TRPGSupportTool/MainDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TRPGSupportTool
+{
+    public partial class MainDisplay : Form
+    {
+        public MainDisplay()
+        {
+            InitializeComponent();
+            SubDisplay f2 = new SubDisplay();
+            f2.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
